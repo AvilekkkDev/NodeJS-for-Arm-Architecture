08cb1609777c2db1665a977470388a0e0fec4ae2
Avilekkk <84607950+AvilekkkDev@users.noreply.github.com>
2021-05-22 07:14:28 +0200

    Add files via upload

---
+++ NodeJS-Armv8.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+wget https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-arm64.tar.xz
+tar -xf node-v14.17.0-linux-arm64.tar.xz
+cd node-v14.17.0-linux-arm64
+sudo cp -R * /usr/local/
+clear
+echo Installed :D
