dea56d7a0ef911686f90c9841dafef5c439c3a2e
Avilekkk <84607950+AvilekkkDev@users.noreply.github.com>
2021-05-22 06:57:00 +0200

    Add files via upload

---
+++ NodeJS-Armv7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+wget https://nodejs.org/dist/v14.17.0/node-v14.17.0-linux-armv7l.tar.xz
+tar -xf node-v14.17.0-linux-armv7l.tar.xz
+cd node-v14.17.0-linux-armv7l
+sudo cp -R * /usr/local/
+clear
+echo Installed :D
